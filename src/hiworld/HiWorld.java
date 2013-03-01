d62b167d1804e47d10852d60a375f94ae1c45dc5
Sarah de Voss <satv@itu.dk>
2013-03-01 12:31:09 +0100

    Initial push

---
+++ src/hiworld/HiWorld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hiworld;
+
+/**
+ *
+ * @author Sarah de Voss
+ */
+public class HiWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   //  System.out.println(HelloString("d"));  
+     Static ourNewClass = new Static();
+        String output = ourNewClass.HelloString("Sarah and Elvis");
+        System.out.println(output);
+      
+    }
+
+}
---
+++ src/hiworld/Static.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hiworld;
+
+/**
+ *
+ * @author Sarah de Voss
+ */
+class Static {
+
+    
+      public  String HelloString(String name){
+        String output="Hello " + name;
+        return output;
+    }
+}
